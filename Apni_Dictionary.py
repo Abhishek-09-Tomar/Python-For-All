ad46f116a0e963a59f28c8fddcc9d898073ad923
Abhishek Tomar <164189524+Abhishek-09-Tomar@users.noreply.github.com>
2024-04-30 01:17:08 +0530

    Add files via upload
     To create a dictionary could involve defining key-value pairs using curly braces, like {key1: value1, key2: value2}. This simple syntax efficiently organizes and stores data for quick retrieval and manipulation.

---
+++ Apni_Dictionary.py
@@ -0,0 +1,17 @@
+"""
+Problem Statement : Make a Dictionary that stores some value. (You may store the key-pair values according to
+					your comfortability).
+"""
+
+"""
+Dictionary :  Dictionary is a mutable data structures that allow you to store key-value pairs
+"""
+
+# Define the variable name and store the value of some attributes.
+myDict = {"Name":"Abhishek Tomar", "Course":"Bachelor of Technology", "Institute":"Galgotias University",
+          "Subject Names": {"Engineering Mathematics","Programming Problem solving","Discrete Mathematics"}}
+
+print(myDict.items())  # They print the items present in the dictionary as an output on your console window.
+
+print(myDict) # They print your entire dictionary.
+
